3a89f58635953e953a28387e6d6788e795f120a8
guigmarconi <30665498+guigmarconi@users.noreply.github.com>
2025-08-26 23:43:52 -0300

    Section 11: adding Flyway Migrations.

---
+++ rest-with-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(3, 'São Caetano do Sul - São Paulo - Brasil', 'Guigo', 'Male', 'Marconi'),
+	(4, 'São Bernardo do Campo - São Paulo - Brasil', 'Leonardo', 'Male', 'Larapio'),
+	(5, 'Temple - Texas - EUA', 'Victor', 'Male', 'Mota'),
+	(6, 'Ota - Japan', 'Bruno', 'Male', 'Vetorazo'),
+	(7, 'Porbandar - India', 'Indira', 'Male', 'Gandhi'),
+	(9, 'Tibet', 'Mahatma', 'Binary', 'Glandhi'),
+	(14, 'South Africa', 'Nelson', 'Male', 'Mandela'),
+	(15, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
